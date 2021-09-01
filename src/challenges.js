08a38214ee9f98629fc7535732fbf89ea9cc2e9b
Lucca Neiva <luccaneiva12@gmail.com>
2021-08-31 22:15:09 -0300

    Adicionado desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -108,11 +108,57 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  let returnString = '';
+  for (let letter of string) {
+    switch (letter) {
+      case 'a':
+        returnString += '1';
+        break;
+      case 'e':
+        returnString += '2';
+        break;
+      case 'i':
+        returnString += '3';
+        break;
+      case 'o':
+        returnString += '4';
+        break;
+      case 'u':
+        returnString += '5';
+        break;
+      default:
+        returnString += letter;
+        break;
+    }
+  }
+  return returnString;
 }
-function decode() {
-  // seu código aqui
+function decode(string) {
+  let returnString = '';
+  for (let letter of string) {
+    switch (letter) {
+      case '1':
+        returnString += 'a';
+        break;
+      case '2':
+        returnString += 'e';
+        break;
+      case '3':
+        returnString += 'i';
+        break;
+      case '4':
+        returnString += 'o';
+        break;
+      case '5':
+        returnString += 'u';
+        break;
+      default:
+        returnString += letter;
+        break;
+    }
+  }
+  return returnString;
 }
 
 module.exports = {
