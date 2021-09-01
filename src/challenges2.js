9b09aee335442366f90fdcf1d0af444127d6c337
Lucca Neiva <luccaneiva12@gmail.com>
2021-08-31 22:39:33 -0300

    Adicionado desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,21 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas");
+function techList(array, name) {
+  let sortedArray = array.sort();
+  let returnArray = [];
+  if (array.length == 0){
+    return 'Vazio!';
+  } else {
+    for (let item of sortedArray) {
+      let object = {
+        "tech": item,
+        "name": name
+      };
+  
+      returnArray.push(object);
+    }
+    return returnArray;
+  }
 }
 
 // Desafio 11
